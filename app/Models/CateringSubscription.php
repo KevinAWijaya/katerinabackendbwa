8e4edf50f3026759c12c35787bd8d0a5215c2977
KevinAWijaya <kevinaw46@gmail.com>
2025-09-13 12:22:31 +0700

    bug fix: fillable phone in ateringsubs model

--- app/Models/CateringSubscription.php
+++ app/Models/CateringSubscription.php
@@ -14,6 +14,7 @@ class CateringSubscription extends Model
         'booking_trx_id',
         'name',
         'photo',
+        'phone',
         'email',
         'delivery_time',
         'proof',
